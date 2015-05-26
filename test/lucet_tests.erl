60caf8300ec69acbc4c49d5b2be0180bf30b9d3a
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-05-26 16:03:42 +0200

    Assert patchp wires

--- test/lucet_tests.erl
+++ test/lucet_tests.erl
@@ -26,6 +26,8 @@
                                         not lists:suffix("PatchP", Id)
                                 end, X)).
 
+-define(TYPE(V), #{<<"type">> := #{value := V}}).
+
 %% Tests based on the topology shown in the figure 17A in the Lucet Desgin
 %% document:
 %% https://docs.google.com/document/d/1Gtoi8IX1EN3oWDRPTFa_VltOdJAwRbl_ChAZWk8oKIk/edit#
@@ -62,7 +64,8 @@ it_bounds_path_between_ep_and_ofp() ->
     lucet:wire2(Src, Dst),
 
     %% THEN
-    assert_path_bounded(Src, Dst, ?EP1_TO_PH1_OFS1_OFP2).
+    assert_path_bounded(Src, Dst, ?EP1_TO_PH1_OFS1_OFP2),
+    assert_patch_panels_wired(?EP1_TO_PH1_OFS1_OFP2).
 
 it_finds_path_between_ofps() ->
     %% GIVEN
@@ -84,7 +87,22 @@ it_bounds_path_between_ofps() ->
     lucet:wire2(Src, Dst),
 
     %% THEN
-    assert_path_bounded(Src, Dst, ?PH1_OFS1_OFP1_TO_PH2_OFS1_OFP1).
+    assert_path_bounded(Src, Dst, ?PH1_OFS1_OFP1_TO_PH2_OFS1_OFP1),
+    assert_patch_panels_wired(?PH1_OFS1_OFP1_TO_PH2_OFS1_OFP1).
+
+%% Assertions
+
+assert_path_bounded(Src, Dst, ExpectedPath) ->
+    Path = find_path_to_be_bound(Src, Dst),
+    ?assertEqual(?BOUNDED(ExpectedPath), Path).
+
+assert_patch_panels_wired(UnboundedPath) ->
+    PatchPanelsWires = construct_expected_patch_panels_wires(UnboundedPath),
+    lists:foreach(fun({PatchpId, PortA, PortB}) ->
+                          ActualWires = find_patch_panel_wires(PatchpId),
+                          ?assertEqual(PortB, maps:get(PortA, ActualWires)),
+                          ?assertEqual(PortA, maps:get(PortB, ActualWires))
+                  end, PatchPanelsWires).
 
 %% Internal functions
 
@@ -98,6 +116,28 @@ find_path_to_be_bound(Src, Dst) ->
     Path = lucet:find_path_to_bound(Src, Dst),
     lists:map(fun({Id, _, _}) -> Id end, Path).
 
-assert_path_bounded(Src, Dst, ExpectedPath) ->
-    Path = find_path_to_be_bound(Src, Dst),
-    ?assertEqual(?BOUNDED(ExpectedPath), Path).
+construct_expected_patch_panels_wires(UnboundedPath) ->
+    construct_expected_patch_panels_wires(UnboundedPath, [], []).
+
+construct_expected_patch_panels_wires([IdBin | Rest], Passed, Wires0) ->
+    Id = binary_to_list(IdBin),
+    Wires1 = case lists:suffix("PatchP", Id) of
+                 true ->
+                     PortA = hd(Passed),
+                     PortB = hd(Rest),
+                     [{IdBin, PortA, PortB} | Wires0];
+                 false ->
+                     Wires0
+             end,
+    construct_expected_patch_panels_wires(Rest, [IdBin | Passed], Wires1);
+construct_expected_patch_panels_wires([], _, Wires) ->
+    Wires.
+
+
+
+find_patch_panel_wires(PatchpId) ->
+    Fun = fun(_, ?TYPE(<<"lm_patchp">>) = Md, _, _) ->
+                  #{<<"wires">> := #{value := Wires}} = Md,
+                  {stop, Wires}
+          end,
+    dby:search(Fun, not_found, PatchpId, [breadth, {max_depth, 0}]).
